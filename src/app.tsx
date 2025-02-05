3d39f3eee4b31fefcdbfaf4abdfaa05d00b65948
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-02-05 15:48:41 -0300

    Fix:Ajuste na box do HeroSection

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,18 @@
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 import FeatureSection from "./components/FeatureSection"
+import WorkFlow from "./components/WorkFlow"
 
 const app = () => {
   return (
-   <>
-   <Navbar />
-   <div className="max-w-7xl mx-auto pt-20 px-6">
-   <HeroSection />
-   <FeatureSection />
-   </div>
-   </>
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <WorkFlow />
+      </div>
+    </>
   )
 }
 
