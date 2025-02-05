4cafec684d80105ed75cd240ad826fa7b0777123
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-02-05 15:07:10 -0300

    feat:Criacao da HeroSection

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
-import Navbar from "./components/navbar"
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
 
 const app = () => {
   return (
    <>
    <Navbar />
+   <div className="max-w-7xl mx-auto pt-20 px-6">
+
+   <HeroSection />
+
+   </div>
    </>
   )
 }
